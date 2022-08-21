de6af8f343e319869767b3a5780ea6853c249594
Mohitsaini2028 <mohitsaini00720@gmail.com>
2022-08-21 16:29:05 +0530

    update

--- app.js
+++ app.js
@@ -20,10 +20,7 @@ const io = socketio(server);
 const Chat = require('./models/chat')
 
 
-const DATABASE_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter-clone.5itd4no.mongodb.net/?retryWrites=true&w=majority`;
-
-
-mongoose.connect(DATABASE_URL)
+mongoose.connect(process.env.DATABASE)
 .then(()=>{
     console.log("db connected");
 })
